3dc2d24d1eb530098dfdb69a4f81a54b4c8cb412
Gabriel Niziolek <ganiziolek@gmail.com>
2024-02-09 22:16:31 -0300

    initial wayland config

---
+++ make_ln.sh
@@ -0,0 +1,11 @@
+
+# Hyprland
+mkdir -p ~/.config/hypr
+cd ~/.config/hypr
+ln -s -f ~/dotfiles/.config/hypr/hyprland.conf hyprland.conf
+
+# waybar
+mkdir -p ~/.config/waybar
+cd ~/.config/waybar
+ln -s -f ~/dotfiles/.config/waybar/config config
+ln -s -f ~/dotfiles/.config/waybar/style.css style.css
